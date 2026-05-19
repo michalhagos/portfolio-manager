d6d1f7735ec465cf00591bfac54e10e5983adc63
michalhagos <mhayele3@gmail.com>
2026-05-19 11:01:20 -0500

    Add constructor to Gold

--- src/main/java/com/pluralsight/finance/Gold.java
+++ src/main/java/com/pluralsight/finance/Gold.java
@@ -3,6 +3,9 @@ package com.pluralsight.finance;
 public class Gold extends FixedAsset{
 
     private double weight;
-
-
+    // passes name and marketValue up to FixedAsset using super
+    public Gold(String name, double marketValue, double weight) {
+        super(name, marketValue);
+        this.weight = weight;
+    }
 }
