4bd868af6d09045ab711221b245ab05351be6836
michalhagos <mhayele3@gmail.com>
2026-05-19 10:28:25 -0500

    Implemented getValue method in FixedAsset

---
+++ src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -0,0 +1,22 @@
+package com.pluralsight.finance;
+// making it abstract because there will be no need to create an object from it
+public abstract class FixedAsset implements Valuable {
+    // name of the asset
+    private String name;
+    // current market value in dollars
+    private double marketValue;
+
+    public FixedAsset(String name, double marketValue) {
+        this.name = name;
+        this.marketValue = marketValue;
+    }
+    // returns the market Value it is overriding the method
+    @Override
+    public double getValue() {
+        return marketValue;
+    }
+
+
+
+
+}
--- src/main/java/com/pluralsight/finance/Valuable.java
+++ src/main/java/com/pluralsight/finance/Valuable.java
@@ -1,6 +1,7 @@
 package com.pluralsight.finance;
 
 public  interface Valuable {
+    // Any class that implements Valuable must have this method
     double getValue();
 
 
