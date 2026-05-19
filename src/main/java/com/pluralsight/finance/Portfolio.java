313d1425ec54fc9875021c21e0545db1e6df7885
michalhagos <mhayele3@gmail.com>
2026-05-19 15:53:17 -0500

    Add addAsset method to Portfolio class

--- src/main/java/com/pluralsight/finance/Portfolio.java
+++ src/main/java/com/pluralsight/finance/Portfolio.java
@@ -6,7 +6,15 @@ public class Portfolio {
 
     // a list that holds all the valuable assets in this portfolio
     private ArrayList<Valuable> assets;
+    // constructor initializes the empty assets list
+    public Portfolio() {
+        this.assets = new ArrayList<>();
+    }
 
+    // adds a new asset to the portfolio
+// this means because the list holds Valuable you can add any type of asset
 
-
+    public void addAsset(Valuable asset) {
+        this.assets.add(asset);
+    }
 }
