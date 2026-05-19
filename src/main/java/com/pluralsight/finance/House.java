a46ff69da996add2d3d45a995412e718bf794ac7
michalhagos <mhayele3@gmail.com>
2026-05-19 14:02:04 -0500

    add year built, square feet, bedrooms fields to House

--- src/main/java/com/pluralsight/finance/House.java
+++ src/main/java/com/pluralsight/finance/House.java
@@ -2,4 +2,16 @@ package com.pluralsight.finance;
 
 public class House extends FixedAsset{
 
+
+    // the year the house was built
+    private int yearBuilt;
+    // the size of the house in square feet
+    private int squareFeet;
+    // the number of bedrooms
+    private int bedrooms;
+
+
+
+
+
 }
