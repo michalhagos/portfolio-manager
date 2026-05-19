a8e5221e0a15ca34c93fa8150193a8461b96d46f
michalhagos <mhayele3@gmail.com>
2026-05-19 10:32:40 -0500

    Add getName, getMarketValue, setMarketValue to FixedAsset

--- src/main/java/com/pluralsight/finance/FixedAsset.java
+++ src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -1,4 +1,5 @@
 package com.pluralsight.finance;
+
 // making it abstract because there will be no need to create an object from it
 public abstract class FixedAsset implements Valuable {
     // name of the asset
@@ -10,13 +11,25 @@ public abstract class FixedAsset implements Valuable {
         this.name = name;
         this.marketValue = marketValue;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarketValue() {
+        return marketValue;
+    }
+
+    // Used set method to update the value when market prices change
+    public void setMarketValue(double marketValue) {
+        this.marketValue = marketValue;
+    }
+
     // returns the market Value it is overriding the method
     @Override
     public double getValue() {
         return marketValue;
     }
-
-
 
 
 }
