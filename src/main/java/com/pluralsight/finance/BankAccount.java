a7800c50e0cf214c1e82128a0b91fdc16e5588cd
michalhagos <mhayele3@gmail.com>
2026-05-19 14:18:22 -0500

    Added private fields name accountNumber and balance to BankAccount"

--- src/main/java/com/pluralsight/finance/BankAccount.java
+++ src/main/java/com/pluralsight/finance/BankAccount.java
@@ -2,5 +2,11 @@ package com.pluralsight.finance;
 
 public class BankAccount implements Valuable {
 
+    // the name of the account holder
+    private String name;
+    // the unique account number
+    private String accountNumber;
+    // the current balance in the account
+    private double balance;
 
 }
