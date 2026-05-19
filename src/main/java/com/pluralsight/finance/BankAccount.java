a9daf64fe75cf5b3b30666c44d9ea34fdc1547d8
michalhagos <mhayele3@gmail.com>
2026-05-19 14:24:52 -0500

    added a setters and getters to bankAccount class

--- src/main/java/com/pluralsight/finance/BankAccount.java
+++ src/main/java/com/pluralsight/finance/BankAccount.java
@@ -9,4 +9,42 @@ public class BankAccount implements Valuable {
     // the current balance in the account
     private double balance;
 
+    public BankAccount(String accountNumber, String name, double balance) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // returns the name of the account holder
+    public String getName() {
+        return name;
+    }
+
+    // returns the account number
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // returns the current balance
+    public double getBalance() {
+        return balance;
+    }
+    // updates the name of the account holder
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // updates the account number
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // updates the balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+
+
 }
