2ef94e97034d7c1754aa260120d38f37d74a1c6a
michalhagos <mhayele3@gmail.com>
2026-05-19 14:39:35 -0500

    Added private fields name accountNumber and balance to CreditCard

---
+++ src/main/java/com/pluralsight/finance/CreditCard.java
@@ -0,0 +1,13 @@
+package com.pluralsight.finance;
+
+public class CreditCard implements Valuable{
+
+    // the name of the card-holder
+    private String name;
+    // the unique account number
+    private String accountNumber;
+    // how much is currently owed on the card
+    private double balance;
+
+
+}
