2dbab19b27309c692da1e7f57825d7f3f6f83f55
michalhagos <mhayele3@gmail.com>
2026-05-19 15:47:27 -0500

    add Arrayslist of Valuable with a name assets.

--- src/main/java/com/pluralsight/finance/Portfolio.java
+++ src/main/java/com/pluralsight/finance/Portfolio.java
@@ -1,7 +1,11 @@
 package com.pluralsight.finance;
 
+import java.util.ArrayList;
+
 public class Portfolio {
 
+    // a list that holds all the valuable assets in this portfolio
+    private ArrayList<Valuable> assets;
 
 
 
