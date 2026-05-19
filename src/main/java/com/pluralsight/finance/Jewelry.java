a2e2c0d8b08cb7fd74db096d6f108033de8cbe1c
michalhagos <mhayele3@gmail.com>
2026-05-19 10:40:53 -0500

    Create empty Jewelry class extending FixedAsset, Added karat field to Jewelry,  Added constructor to Jewelry

---
+++ src/main/java/com/pluralsight/finance/Jewelry.java
@@ -0,0 +1,15 @@
+package com.pluralsight.finance;
+
+public class Jewelry extends FixedAsset {
+    // example of karat like 24, 18
+    private double karat;
+
+    // Passes name and marketValue from FixedAsset class
+    public Jewelry(String name, double marketValue, double karat) {
+        // using super to use the parent class constructor
+        super(name, marketValue);
+        this.karat = karat;
+    }
+
+
+}
